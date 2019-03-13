46ef7d0eb157818de9a48049530d81afa64bd88d
Kevin Chin <chinkevin25@gmail.com>
2019-03-12 22:16:48 -0700

    Fix path for SRC and PUBLIC dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/client/src');
-const PUBLIC_DIR = path.join(__dirname, '/client/public');
+const SRC_DIR = path.join(__dirname, '/src');
+const PUBLIC_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
