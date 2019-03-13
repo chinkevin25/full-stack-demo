34205d79171ed2836f956bb560bedfd66c00839a
Kevin Chin <chinkevin25@gmail.com>
2019-03-12 22:09:49 -0700

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const PUBLIC_DIR = path.join(__dirname, '/client/public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: PUBLIC_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
