35d5d7e6f7af6f9342cd8a967fc5749a5eb71dba
Kevin Chin <chinkevin25@gmail.com>
2019-03-12 19:34:05 -0700

    Implement simple get route

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@
 const express = require('express');
 
 const app = express();
-
 // Configure the port with app.set
 app.set('port', process.env.PORT || 3000);
+
+app.get('/api/cohort/:cohort', (request, response) => {
+  console.log(`Registered a ${request.method} at ${request.url}`);
+  const cohort = request.params.cohort;
+  const retort = ['113', '114', '107'].includes(cohort)
+    ? `HRSF-${cohort} is the best`
+    : `HRSF-${cohort} is the worst`;
+  response.send(retort);
+});
 
 // Lastly, we use app.listen to bind our application to a port and have the application
 // listen for incoming requests on that port number.
