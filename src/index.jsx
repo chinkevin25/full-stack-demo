a242350226f9ad3d8d73dbca7e751da2396c831a
Kevin Chin <chinkevin25@gmail.com>
2019-03-12 22:55:18 -0700

    Import App and Render App to DOM

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World From React</h1>, document.getElementById('app'));
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
