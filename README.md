ddeb3f2d3a792dbffe712af337d901845422dab7
Kevin Chin <chinkevin25@gmail.com>
2019-03-15 15:05:41 -0700

    Update README

--- README.md
+++ README.md
@@ -12,4 +12,14 @@ Many applications we build will need to support CRUD operations. CRUD stands for
 
 **D**elete: Delete something (DELETE)
 
-Let's refactor our server to support these routes.
+Let's refactor our server to support these routes. While were are refactoring, let's introduce some popular and useful libraries.
+
+# Useful libraries
+
+```sh
+npm i -S morgan cors
+```
+
+`morgan` is a library that helps us log incoming HTTP requests. The documentation can be found [here](https://www.npmjs.com/package/morgan).
+
+`cors` is a library that helps us manage our cross origin request permissions. The documentation can be found [here](https://www.npmjs.com/package/cors)
--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
 const express = require('express');
-// import path so we can use the library to help us handle the creation of paths.
 const path = require('path');
+// morgan is a HTTP request logger middleware for node.js. This helps log details of the requests
+// that are being sent to the server. This is extremely useful to see if our server is responding
+// correctly.
+const morgan = require('morgan');
+// cors is a useful library that helps handle our cross origin requests for us!
+//  Refer to the documentation to see what options are available.
+const cors = require('cors');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-// This allows us to serve our static files in public (html, images, js files, bundle.js) easily
-// The assets will be sent whenever there is a request to the endpoint '/'.
 app.use('/', express.static(path.join(__dirname, '../public')));
+// Initialize morgan as a middleware in the 'dev' mode. Refer to the documentation for more details.
+app.use(morgan('dev'));
+// Initalize cors as a middleware in the default mode. This will enable CORS for all origins
+app.use(cors());
 
-app.get('/api/cohort/:cohort', (request, response) => {
-  console.log(`Registered a ${request.method} at ${request.url}`);
-  const cohort = request.params.cohort;
-  const retort = ['113', '114', '107'].includes(cohort)
-    ? `HRSF-${cohort} is the best`
-    : `HRSF-${cohort} is the worst`;
-  response.send(retort);
+app.get('/', (req, res) => {
+  res.send('This is our GET route');
+});
+
+app.post('/', (req, res) => {
+  res.send('This is our POST route');
+});
+
+app.put('/', (req, res) => {
+  res.send('This is our UPDATE route');
+});
+
+app.delete('/', (req, res) => {
+  res.send('This is our DELETE route');
 });
 
 app.listen(app.get('port'), () => {
