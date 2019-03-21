6333c412f5a5d5a31966de923238e3f99a7b9cc9
Kevin Chin <chinkevin25@gmail.com>
2019-03-21 14:17:23 -0700

    Implement fetching all and posting to database

--- server/server.js
+++ server/server.js
@@ -1,51 +1,50 @@
 const express = require('express');
 const path = require('path');
-// morgan is a HTTP request logger middleware for node.js. This helps log details of the requests
-// that are being sent to the server. This is extremely useful to see if our server is responding
-// correctly.
 const morgan = require('morgan');
-// cors is a useful library that helps handle our cross origin requests for us!
-// Refer to the documentation to see what options are available.
 const cors = require('cors');
+// When we require this file, nodeJS needs to run the file. When this file is ran,
+// we connect to our database and sage the connection object to the variable db.
+const db = require('./db');
+// require the models index.js file so we can interact with our database
+const models = require('./model');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use('/', express.static(path.join(__dirname, '../public')));
-// Initialize morgan as a middleware in the 'dev' mode. Refer to the documentation for more details.
 app.use(morgan('dev'));
-// Initalize cors as a middleware in the default mode. This will enable CORS for all origins
 app.use(cors());
-
-// Notice that this middleware is not in our package.json. This comes free with express.
-// express.json is a middleware based on body-parser and will help us parse JSON data and
-// automatically add the information to req.body
 app.use(express.json());
 
-// Let's create a non-persistent datastore until we introduce databases!
-const messages = [];
-
 app.get('/api/messages/', (req, res) => {
-  // Send all the messages back to the requester
-  res.send(messages);
+  // Make an async request to our database and fetch everything!
+  // Model.find() with no arguments is the analogous to SELECT * FROM Messages in SQL.
+  models.Messages.find()
+    .then((messages) => {
+      // Send a status code and send the payload back to the server
+      res.status(200).send(messages);
+    });
 });
 
 app.post('/api/messages', (req, res) => {
-  const newMessage = {
+  // Make an asnyc request to our database and create our new document!
+  models.Messages.create({
     text: req.body.text,
-    createdAt: new Date(),
-  };
-  // Add message to our in-memory (non-persistent datastore)
-  messages.push(newMessage);
-  res.status(201); // Status code 201 means item has successfuly been created.
-
-  // It's common for the item that has been created to be returned back to the
-  // requester. Often the server will add some extra information (e.g. createdAt)
-  // and then save it to the datastore.
-
-  // Typically it's not kosher to send the entire datastore back to the client
-  // in a POST request. Handle reading from the datastore with a GET request.
-  res.send(newMessage);
+  })
+    .then((message) => {
+      res.status(201).send(message);
+    });
+  // Notice that the we are no longer decorating our incoming object with the date anymore!
+  // This is because we set a default value for our createdAt field in our model. If we were
+  // log the value of message coming back from the database it would look like:
+  /*
+    {
+      "_id" : ObjectId("5c93fc10632ee97aecbea09b"),
+      "text" : "Hello World",
+      "createdAt" : ISODate("2019-03-21T21:03:12.337Z"),
+      "__v" : 0
+    }
+   */
 });
 
 app.put('/api/messages', (req, res) => {
